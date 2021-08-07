8258c85052ad265dfa062a6f157a493911920189
CauaHenrique19 <cauah6002@gmail.com>
2021-08-07 01:27:26 -0300

    Adding funcionality of coment in avaliation

--- src/Pages/AvaliationDetailed/index.js
+++ src/Pages/AvaliationDetailed/index.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { Context } from '../../context/context'
 import Loading from '../../Components/Loading'
 import Coment from '../../Components/Coment'
 import api from '../../services/api'
@@ -6,14 +7,16 @@ import './avaliationDetailed.css'
 
 const AvaliationDetailed = (props) => {
 
+    const { user } = useContext(Context)
     const [loading, setLoading] = useState(true)
     const [avaliationId, setAvaliationId] = useState(0)
     const [avaliationDetailed, setAvaliationDetailed] = useState({})
+    const [contentComent, setContentComent] = useState('')
+    const [coments, setComents] = useState([])
 
     useEffect(() => {
         setLoading(true)
         setAvaliationId(props.match.params.id)
-
     }, [props])
     
     useEffect(() => {
@@ -22,13 +25,36 @@ const AvaliationDetailed = (props) => {
             .then(res => {
                 if(res.data.avaliation){
                     setAvaliationDetailed(res.data)
+                    setComents(res.data.coments)
                     setLoading(false)
-                    console.log(avaliationDetailed.coments)
                 }
             })
             .catch(error => console.error(error.message))
         }
     }, [avaliationId])
+
+    function handleComent(){
+        const coment = { user_id: user.id, avaliation_id: avaliationId, content: contentComent }
+        
+        api.post('/coments', coment)
+            .then(res => {
+                const updatedComent = {
+                    ...res.data.coment[0],
+                    user_name: user.name, 
+                    user_id: user.id, 
+                    user_user: user.user,
+                    media_name: avaliationDetailed.media_name,
+                    category_icon: avaliationDetailed.avaliation.category_icon,
+                    category_color: avaliationDetailed.avaliation.category_color,
+                    amountComents: 0,
+                    amountLikes: 0
+                }
+                console.log(updatedComent)
+                setComents([updatedComent, ...coments])
+            })
+            .catch(error => console.error(error.message))
+        
+    }
 
     return (
         <div className="avaliation-detail-container">
@@ -83,15 +109,27 @@ const AvaliationDetailed = (props) => {
                         </div>
                     </div>
                 }
+                <div className="form-coment">
+                    <h1>Comente</h1>
+                    <textarea 
+                        value={contentComent} 
+                        onChange={e => setContentComent(e.target.value)} 
+                        placeholder="O que você acha?" cols="30" rows="10" maxLength="360">
+                    </textarea>
+                    <button onClick={() => handleComent()}>
+                        <ion-icon name="chatbox"></ion-icon>
+                        Comentar
+                    </button>
+                </div>
                 <div className="avaliations-coments-container">
                     {
-                        avaliationDetailed.coments && avaliationDetailed.coments.length > 0 && 
+                        coments.length > 0 && 
                         <div className="header-avaliations-coments-container">
                             <h1>Comentários</h1>
                         </div>
                     }
                     {
-                        avaliationDetailed.coments && avaliationDetailed.coments.length === 0 &&
+                        coments.length === 0 &&
                         <div className="nothing-container">
                             <ion-icon name="alert-circle-outline"></ion-icon>
                             <h1>Nenhum comentário!</h1>
@@ -99,54 +137,8 @@ const AvaliationDetailed = (props) => {
                     }
                     <div className="coments">
                         {
-                            avaliationDetailed.coments && avaliationDetailed.coments.length > 0 && avaliationDetailed.coments.map(coment => (
+                            coments.length > 0 && coments.map(coment => (
                                 <Coment key={coment.id} coment={coment} />
-                                // <div className="coment">
-                                //     <div className="header-coment">
-                                //         <div className="info-user">
-                                //             <svg xmlns="http://www.w3.org/2000/svg" width="51" height="40" viewBox="0 0 51 40"
-                                //                 fill={avaliationDetailed.avaliation.category_color}>
-                                //                 <path
-                                //                     d="M1.84167 23.1579L16.575 0H24.65L15.1583 20.0702C19.0306 22.0351 20.9667 25.1696 20.9667 29.4737C20.9667 32.2807 19.9278 34.7602 17.85 36.9123C15.7722 38.9708 13.3167 40 10.4833 40C7.46111 40 4.95833 38.9708 2.975 36.9123C0.991667 34.8538 0 32.3743 0 29.4737C0 27.1345 0.613889 25.0292 1.84167 23.1579ZM28.1917 23.1579L42.925 0H51L41.5083 20.0702C45.3806 22.0351 47.3167 25.1696 47.3167 29.4737C47.3167 32.2807 46.2778 34.7602 44.2 36.9123C42.1222 38.9708 39.6667 40 36.8333 40C33.8111 40 31.3083 38.9708 29.325 36.9123C27.3417 34.8538 26.35 32.3743 26.35 29.4737C26.35 27.1345 26.9639 25.0292 28.1917 23.1579Z">
-                                //                 </path>
-                                //             </svg>
-                                //             <div className="user-info">
-                                //                 <h3>{coment.name}</h3>
-                                //                 <p>@{coment.user}</p>
-                                //             </div>
-                                //         </div>
-                                //         <div className="info-post">
-                                //             <p style={{ backgroundColor: avaliationDetailed.avaliation.category_color }}>{coment.created_at}</p>
-                                //         </div>
-                                //     </div>
-                                //     <div className="content-coment">{coment.content}</div>
-                                //     <div className="footer-coment">
-                                //         <div className="info-media">
-                                //             <div style={{ backgroundColor: avaliationDetailed.avaliation.category_color }} className="color-coment">
-                                //                 <ion-icon name={avaliationDetailed.avaliation.category_icon}></ion-icon>
-                                //             </div>
-                                //             <div className="info-footer">
-                                //                 <h3>Sobre</h3>
-                                //                 <p>{avaliationDetailed.avaliation.media_name}</p>
-                                //             </div>
-                                //         </div>
-                                //         <div className="info-avaliation">
-                                //             <div style={{ backgroundColor: avaliationDetailed.avaliation.category_color }} className="amount-coments">
-                                //                 <ion-icon name="chatbubble"></ion-icon>
-                                //                 <p>0</p>
-                                //             </div>
-                                //             <div style={{ backgroundColor: avaliationDetailed.avaliation.category_color }} className="amount-likes">
-                                //                 <ion-icon name="heart"></ion-icon>
-                                //                 <p>0</p>
-                                //             </div>
-                                //         </div>
-                                //     </div>
-                                //     <div className="links-coment-container">
-                                //         <button>
-                                //             <ion-icon name="heart-outline"></ion-icon>
-                                //         </button>
-                                //     </div>
-                                // </div>
                             ))
                         }
                     </div>
