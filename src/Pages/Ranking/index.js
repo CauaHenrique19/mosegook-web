e3eb5bdd37a793a11bc18b46ee16d222780a4e39
CauaHenrique19 <cauah6002@gmail.com>
2021-09-05 20:37:46 -0300

    Make requisition to api and render data

--- src/Pages/Ranking/index.js
+++ src/Pages/Ranking/index.js
@@ -1,61 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { Context } from '../../context/context'
 
-import userImage from '../../assets/user-image.png'
+import api from '../../services/api'
+
 import './style.css'
 
 const Ranking = () => {
+
+    const { user } = useContext(Context) 
+
+    const [ranks, setRanks] = useState([])
+    const [top, setTop] = useState([])
+    const [rankStatus, setRankStatus] = useState({})
+
+    useEffect(() => {
+        api.get('/ranks')
+            .then(res => setRanks(res.data))
+            .catch(error => console.error(error.message))
+
+        api.get('/ranks/top')
+            .then(res => setTop(res.data))
+            .catch(error => console.error(error.message))
+
+        api.get(`/users/rank/${user.id}`)
+            .then(res => setRankStatus(res.data))
+            .catch(error => console.error(error.message))
+    }, [user])
+
     return (
         <div className="ranking-container">
             <div className="ranking-content-container">
                 <h1>Ranking</h1>
                 <div className="ranking-upload">
-                    <div className="ranking-upload-progress">
-                        <div className="upload-progress">
-
-                        </div>
+                    <div className="ranking-upload-progress" >
+                        <div className="upload-progress" style={{ width: rankStatus.percentualToNextRank }} ></div>
                     </div>
                     <div className="ranks">
-                        <div className="rank">
-                            <div className="rank-circle"></div>
-                            <h3>Iniciante</h3>
-                        </div>
-                        <div className="rank">
-                            <div className="rank-circle"></div>
-                            <h3>Casual</h3>
-                        </div>
-                        <div className="rank">
-                            <div className="rank-circle"></div>
-                            <h3>Expectador</h3>
-                        </div>
-                        <div className="rank">
-                            <div className="rank-circle"></div>
-                            <h3>Fã</h3>
-                        </div>
-                        <div className="rank">
-                            <div className="rank-circle"></div>
-                            <h3>Profissional</h3>
-                        </div>
-                        <div className="rank">
-                            <div className="rank-circle"></div>
-                            <h3>Geek</h3>
-                        </div>
-                        <div className="rank">
-                            <div className="rank-circle"></div>
-                            <h3>Expert</h3>
-                        </div>
+                        {
+                            ranks &&
+                            ranks.map((rank, index) => (
+                                <div className="rank" >
+                                    <div className="rank-circle" style={{ borderWidth: 2, borderStyle: 'solid', borderColor: rank.color }}>
+                                        <h2>{index + 1}</h2>
+                                    </div>
+                                    <h3>{rank.name}</h3>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
             <div className="ranking-top-container">
-                <div className="top second">
-                    <img src={userImage} alt="" />
-                </div>
-                <div className="top first">
-                    <img src={userImage} alt="" />
-                </div>
-                <div className="top third">
-                    <img src={userImage} alt="" />
-                </div>
+                {
+                    top &&
+                    top.map(user => (
+                        <div className={`top ${user.position}`}>
+                            <img src={user.url_image} alt={user.user_name} />
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
